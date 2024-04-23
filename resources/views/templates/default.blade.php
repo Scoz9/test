86bfbe2022eab8e087ad325e31e5b185b97690af
Vincenzo Scozzari <vincenzo.scoz9@gmail.com>
2024-04-23 15:17:06 +0200

    Add Login and Register

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,7 +9,8 @@
     <meta name="generator" content="Jekyll v4.1.1">
     <title>@yield('title', 'Home')</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <style>
         main.container {
@@ -17,6 +19,7 @@
     </style>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="d-flex flex-column h-100">
 
     <header>
@@ -25,25 +28,52 @@
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Fixed navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-            <div class="navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{route('albums.index')}}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('albums.create')}}">New Album</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                        </li>
+                <div class="navbar-collapse" id="navbarCollapse">
+                    @auth
+                        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="{{ route('albums.index') }}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('albums.create') }}">New Album</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    @endauth
+                    <ul class="nav navbar-nav navbar-right">
+                        @guest()
+                            <li>
+                                <a class="nav-link" href="{{ route('login') }}"> Login </a>
+                            </li>
+                            <li>
+                                <a class="nav-link" href="{{ route('register') }}"> Register </a>
+                            </li>
+                        @endguest
+                        @auth
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle  nav-link" data-bs-toggle="dropdown" role="button"
+                                    aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                            {{ csrf_field() }}
+                                            <button class="btn btn-default">Logout</button>
+                                        </form>
+                                    </li>
+
+                                </ul>
+                            </li>
+                        @endauth
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </nav>
@@ -51,10 +81,13 @@
 
     <main role="main" class="container">
         @yield('content')
-        {{$slot ?? ''}}
+        {{ $slot ?? '' }}
     </main><!-- /.container -->
     @section('footer')
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
     @show
-    </body>
+</body>
+
 </html>
